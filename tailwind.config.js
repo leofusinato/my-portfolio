6223df1c9440ab5c9caa1b1666637e9ade5bc4df
leofusinato <leonardo.alex.fusinato@gmail.com>
2024-11-11 23:38:33 -0300

    feat: tailwind colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        red: "#E3646E",
+        purple: "#BB72E9",
+        blue: "#3996DB",
+        green: "#82BC4F",
+        yellow: "#EABD5F",
+        gray: {
+          100: "#0D0E11",
+          200: "#16181D",
+          300: "#292C34",
+          400: "#878EA1",
+          500: "#C0C4CE",
+          600: "#E2E4E9",
+        },
+      },
+    },
   },
   plugins: [],
 };
