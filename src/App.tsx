73458ce657ffa5b28ce06479d4892c083af8ca7a
leofusinato <leonardo.alex.fusinato@gmail.com>
2024-11-11 23:50:40 -0300

    feat: icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import "./App.css";
+import { Icons } from "./assets/icons";
 
 function App() {
-  return <div></div>;
+  return (
+    <div className="flex flex-1 justify-center items-center bg-gray-300">
+      <img src={Icons.CaretDoubleDown} />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/assets/icons/index.tsx
@@ -0,0 +1,21 @@
+import ArrowUpRight from "./arrow-up-right.svg";
+import CaretDoubleDown from "./caret-double-down.svg";
+import Devices from "./devices.svg";
+import EnvelopeSimple from "./envelope-simple.svg";
+import GithubLogo from "./github-logo.svg";
+import HardDrives from "./hard-drives.svg";
+import Infinity from "./infinity.svg";
+import InstagramLogo from "./instagram-logo.svg";
+import LinkedinLogo from "./linkedin-logo.svg";
+
+export const Icons = {
+  ArrowUpRight,
+  CaretDoubleDown,
+  Devices,
+  EnvelopeSimple,
+  GithubLogo,
+  HardDrives,
+  Infinity,
+  InstagramLogo,
+  LinkedinLogo,
+};
